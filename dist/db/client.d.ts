3442b414e6b45db022d0ba119da36b721ad343ba
Suvro Bose <bose.suvro.com>
2025-03-20 20:53:44 +0530

    Fixed prisma client

---
+++ dist/db/client.d.ts
@@ -0,0 +1,2 @@
+import { PrismaClient } from "./client/edge";
+export declare const prisma: PrismaClient<import("./client").Prisma.PrismaClientOptions, never, import("./client/runtime/library").DefaultArgs>;
--- dist/prisma/client.d.ts
+++
@@ -1,2 +0,0 @@
-import { PrismaClient } from "./client/index";
-export declare const prisma: PrismaClient<import("./client/index").Prisma.PrismaClientOptions, never, import("./client/index").Prisma.RejectOnNotFound | import("./client/index").Prisma.RejectPerOperation | undefined, import("./client/runtime").DefaultArgs>;
